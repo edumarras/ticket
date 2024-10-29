5cb10ad9c9e4ffe5aa7151455e658288ba8cbd5c
Bianca Andrade <62192157+AndradeBia@users.noreply.github.com>
2024-10-29 16:36:44 -0300

    Update main.py

--- back/mid/main.py
+++ back/mid/main.py
@@ -9,7 +9,7 @@ import httpx
 app = FastAPI()
 print("Middleware iniciado com código atualizado")
 
-# GAYSSAURO
+# GAYSSAURO2
 
 # Configuração do cliente HTTP
 BACKEND_URL = os.getenv("BACKEND_URL", "http://backend:3000")  # Alterado para 'backend'
